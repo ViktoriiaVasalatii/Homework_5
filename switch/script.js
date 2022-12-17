7e367addb0b6ea8076706aa1c1775fd04e21a5a1
Viktoriia <vacalatyvika02@gmail.com>
2022-12-17 15:12:55 +0200

    fixed errors

--- switch/script.js
+++ switch/script.js
@@ -1,19 +1,17 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr);
 
-switch (numOrStr) {
+switch (true) {
     case null:
         console.log('ви скасували')
         break;
     case numOrStr.trim() === '':
         console.log('Empty String');
         break;
-    case isNaN(+numOrStr) && numOrStr:
+    case isNaN(+numOrStr):
         console.log('number is Ba_NaN')
         break;
     default:
         console.log('OK!')
         break;
-} 
-
-
+}
